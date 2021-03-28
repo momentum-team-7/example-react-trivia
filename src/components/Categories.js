488a446d192ef9fd9ac01fc1e3cf963a283a2aa8
Amy Gori <amydgori@gmail.com>
2021-03-28 11:35:41 -0400

    Upgrade styling

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,13 +1,14 @@
 import 'tachyons'
+import '../App.css'
 
 const Categories = ({ categories, setSelectedCategory }) => {
   return (
-    <div className="categories ph3 mt4">
+    <main className="categories ph3 mt4 cf pa2 flex flex-wrap">
       {categories.map((category) => {
         return (
-          <div className="category" key={category.id}>
+          <div className="category " key={category.id}>
             <button
-              className="f6 link dim ba ph3 pv2 mb2 dib black"
+              className="f6 link hover-bg-light-pink hover-dark-pink b ba bg-dark-gray ph3 pv2 dib white square"
               onClick={() => setSelectedCategory(category)}
             >
               {category.name}
@@ -15,7 +16,7 @@ const Categories = ({ categories, setSelectedCategory }) => {
           </div>
         )
       })}
-    </div>
+    </main>
   )
 }
 
