416a189aeba4c35c5b2bc750cfb1476c5c5e9732
Amy Gori <amydgori@gmail.com>
2021-03-29 22:21:43 -0400

    Add confetti component

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,11 +1,12 @@
+import Confetti from 'react-confetti'
+
 const Score = ({ numQuestions, score }) => {
-  console.log('score renders')
   return (
     <>
       <h2>
         Congrats, you scored {score} out of {numQuestions}!
       </h2>
-      <button>Play again?</button>
+      <Confetti />
     </>
   )
 }
